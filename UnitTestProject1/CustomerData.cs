19dc46e42c00e554fe6e59b2136a430b7d1f0a62
hungudym <hungudgm@gmail.com>
2017-07-24 18:27:27 +0700

    upt test

---
+++ UnitTestProject1/CustomerData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [Serializable]
+    public class CustomerData
+    {
+        public decimal SQ_ID { get; set; }
+        public string CTM_CODE { get; set; }
+
+        public string EMAIL { get; set; }
+
+        public string MOBILE { get; set; }
+
+        public decimal REF_SPC_006_GP_ID { get; set; }
+
+        public decimal COUNT_INIT_FLIGHT { get; set; }
+        public decimal COUNT_FLIGHT_STATUS_3 { get; set; }
+        public decimal COUNT_FLIGHT_INTL_STATUS_3 { get; set; }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string customer_data = @"{
+    'SQ_ID': 61,
+    'CTM_CODE': '1023',
+    'EMAIL': 'hungudgm@gmail.com',
+    'MOBILE': '0989700689',
+    'REF_SPC_006_GP_ID': 0,
+    'COUNT_INIT_FLIGHT': 0,
+    'COUNT_FLIGHT_STATUS_3': 44,
+    'COUNT_FLIGHT_INTL_STATUS_3': 9
+}";
+
+
+
+
+
+            dynamic customer = Newtonsoft.Json.JsonConvert.DeserializeObject(customer_data);
+
+            var customer_data_object = Slapper.AutoMapper.MapDynamic<CustomerData>(customer);
+
+
+
+        }
+    }
+}
